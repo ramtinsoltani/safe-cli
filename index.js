a0fc36c053b4f280b66f255ea8dda6ace5387559
Ramtin Soltani <ramtinsoltani93@gmail.com>
2017-10-08 18:18:41 -0700

    Removed temp flag for encryption, added reveal flag, added the generate command to generate random passwords, updated the documentation, new version 1.1.2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const program = require('commander');
 const fs = require('fs-extra');
 const input = require('readline-sync');
 const crypto = require('crypto');
+const clipboard = require('copy-paste');
 
 process.stdin.resume();
 
@@ -46,7 +47,7 @@ let encrypt = (file, options) => {
 
       }
 
-      let password = input.question('Key: ', { hideEchoBack: true });
+      let password = input.question('Key: ', { hideEchoBack: !(options.R || options.reveal) });
       let cipher = crypto.createCipher('aes-256-ctr', password);
       let encrypted = cipher.update(data, 'utf8', 'hex') + cipher.final('hex');
       let destination = `${file}.safe`;
@@ -60,18 +61,6 @@ let encrypt = (file, options) => {
         if ( options.S || options.swap ) {
 
           fs.removeSync(file);
-
-        }
-        else if ( options.T || options.temp ) {
-
-          process.on('exit', fs.removeSync.bind(null, destination));
-          process.on('SIGINT', fs.removeSync.bind(null, destination));
-          process.on('SIGUSR1', fs.removeSync.bind(null, destination));
-          process.on('SIGUSR2', fs.removeSync.bind(null, destination));
-
-          input.question('PRESS ENTER TO DELETE THE ENCRYPTED FILE...\n');
-
-          process.exit();
 
         }
 
@@ -96,6 +85,16 @@ let decrypt = (file, options) => {
   if ( file.substr(file.length - 5) !== '.safe' ) {
 
     console.error('Only SAFE files can be decrypted!');
+
+    process.exit();
+
+    return;
+
+  }
+
+  if ( (options.T || options.temp) && (options.S || options.swap) ) {
+
+    console.error('The temp and swap flags cannot be used at the same time!');
 
     process.exit();
 
@@ -139,7 +138,7 @@ let decrypt = (file, options) => {
 
       }
 
-      let password = input.question('Key: ', { hideEchoBack: true });
+      let password = input.question('Key: ', { hideEchoBack: !(options.R || options.reveal) });
       let decipher = crypto.createDecipher('aes-256-ctr', password);
       let decrypted = decipher.update(data, 'hex', 'utf8') + decipher.final('utf8');
       let destination = file.substr(0, file.length - 5);
@@ -184,19 +183,96 @@ let decrypt = (file, options) => {
 
 };
 
+let generate = (length, options) => {
+
+  length = parseInt(length);
+
+  if ( ! length || length < 1 ) length = 10;
+
+  if ( (options.U || options.upper) && (options.L || options.lower) ) {
+
+    console.error('The lower and upper flags cannot be used at the same time!');
+
+    process.exit();
+
+    return;
+
+  }
+
+  let finalSet = [];
+  let generated = '';
+  let characterSets = {
+
+    lower: 'abcdefghijklmnopqrstuvwxyz',
+    upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    digits: '0123456789',
+    special: `\`~!@#$%^&*()-=_+,.<>/?;:'"[]{}\\|`,
+    space: ' '
+
+  };
+
+  if ( options.L || options.lower ) delete characterSets.upper;
+  if ( options.U || options.upper ) delete characterSets.lower;
+  if ( options.N || options.nodigits ) delete characterSets.digits;
+  if ( ! options.S && ! options.special ) delete characterSets.special;
+  if ( ! options.W && ! options.space ) delete characterSets.space;
+
+  for ( let set in characterSets ) {
+
+    finalSet = finalSet.concat(characterSets[set].split(''));
+
+  }
+
+  while ( generated.length < length ) {
+
+    let random = Math.round(Math.random() * (finalSet.length - 1));
+
+    generated += finalSet[random];
+
+  }
+
+  if ( options.C || options.copy ) {
+
+    clipboard.copy(generated);
+
+    console.log('The generated string was copied to clipboard');
+
+  }
+  else {
+
+    console.log(generated);
+
+  }
+
+  process.exit();
+
+};
+
 program
   .command('encrypt <file>')
   .description('Encrypts the given file')
-  .option('-t, --temp', 'Keeps the process alive and deletes the encrypted file after user hits ENTER')
   .option('-s, --swap', 'Swaps the original file with the encrypted result')
+  .option('-r --reveal', 'Reveals the user password input')
   .action(encrypt);
 
 program
   .command('decrypt <file>')
-  .description('Decripts the given file')
+  .description('Decrypts the given file')
   .option('-t, --temp', 'Keeps the process alive and deletes the decrypted file after user hits ENTER')
   .option('-s, --swap', 'Swaps the original file with the decrypted result')
+  .option('-r --reveal', 'Reveals the user password input')
   .action(decrypt);
+
+program
+  .command('generate [length]')
+  .description('Generates a random string including lower and upper case characters and digits, useful for passwords')
+  .option('-c --copy', 'Copies the generated string to clipboard')
+  .option('-s --special', 'The generated string might include special characters')
+  .option('-n --nodigits', 'The generated string won\'t include digits')
+  .option('-u --upper', 'Uppercase letters only')
+  .option('-l --lower', 'Lowercase letters only')
+  .option('-w --space', 'The generated string might include spaces')
+  .action(generate);
 
 program.parse(process.argv);
 
--- readme.md
+++ readme.md
@@ -10,7 +10,26 @@ Safe CLI is a simple Command Line Integration tool which encrypts and decrypts U
 
 - **Encryption:** Use `safe encrypt <file>` to encrypt a file at the given path (if the path includes spaces, wrap it with double-quotations). An encrypted file will be created at the same directory with the .safe extension.
   - **Swapping:** You can swap the original file with the encrypted .safe file by using either `-s` or `--swap` flags.
-  - **Temporary Encryption:** When using the `-t` or `--temp` flags, the process will be kept open until the user hits <kbd>ENTER</kbd> or kills the process, at which the encrypted file will be deleted. (this feature makes more sense when decrypting).
+  - **Reveal Key:** Using the `-r` or `--reveal` flags will show the key input.
 - **Decryption:** Use `safe decrypt <file>` to decrypt a file at the given path (the file must have the .safe extension). The decrypted file with the original extension will be created at the same directory.
-  - **Swapping:** You can swap the original file with the decrypted .safe file by using either `-s` or `--swap` flags.
-  - **Temporary Decryption:** When using the `-t` or `--temp` flags, the process will be kept open until the user hits <kbd>ENTER</kbd> or kills the process, at which the decrypted file will be deleted.
+  - **Swapping:** You can swap the original file with the decrypted .safe file by using either `-s` or `--swap` flags (exclusive to the temporary flags).
+  - **Temporary Decryption:** When using the `-t` or `--temp` flags, the process will be kept open until the user hits <kbd>ENTER</kbd> or kills the process, at which the decrypted file will be deleted (exclusive to the swap flags).
+  - **Reveal Key:** Using the `-r` or `--reveal` flags will show the key input.
+- **Generating Passwords:** Use `safe generate [length]` to generate a random string. By default, the string is 10 characters long, and contains letters (both uppercase and lowercase) and digits. You can use the following flags to change this:
+  - **Uppercase Only:** `-u` or `--upper` (exclusive to the lowercase flags)
+  - **Lowercase Only:** `-l` or `--lower` (exclusive to the uppercase flags)
+  - **No Digits:** `-n` or `--nodigits`
+  - **Special Characters:** `-s` or `--special`
+  - **Spaces:** `-w` or `--space`
+  - **Copy to Clipboard:** `-c` or `--copy`
+
+## Examples
+
+- Encrypt file at "D:/passwords.md" and remove the original/unencrypted file:  
+    `safe encrypt D:/passwords.md --swap`
+- Decrypt file at "D:/passwords.md.safe" and remove the encrypted file:  
+    `safe decrypt D:/passwords.md.safe --swap`
+- Temporarily decrypt the file at "D:/passwords.md.safe":  
+    `safe decrypt D:/passwords.md.safe --temp`
+- Generate a password (12 characters long, all letters, digits, and special characters):  
+    `safe generate 12 --special`
